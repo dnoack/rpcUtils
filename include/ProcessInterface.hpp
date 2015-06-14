3e102b0a6703b9308b701f5d2949302542d59d56
Dave <Dave@linux-1sjv.fritz.box>
2015-06-14 13:59:05 +0200

    Added doxygen to ComPointB, ProcessInterface and ProcessInterfaceB.

--- include/ProcessInterface.hpp
+++ include/ProcessInterface.hpp
@@ -1,10 +1,3 @@
-/*
- * ProcessInterface.hpp
- *
- *  Created on: 30.05.2015
- *      Author: Dave
- */
-
 #ifndef PROCESSINTERFACE_HPP_
 #define PROCESSINTERFACE_HPP_
 
@@ -13,11 +6,19 @@
 #include "OutgoingMsg.hpp"
 #include "Error.hpp"
 
-
+/**
+ * \interface ProcessInterface
+ * ProccessInterface is a interface to connect classes with functions which should be remotely executed through
+ * a ComPoint. Every class which inherits from ProcessInterface has to implemented the function
+ * process(IncommingMsg*), where the the received message can be parsed and the corresponding RPC executed.
+ * The public method processMsg(IncommingMsg*) is the interface for the overlying ComPoint.
+ */
 class ProcessInterface{
 
 
 	public:
+
+		/** Base-Constructor.*/
 		ProcessInterface()
 		{
 			pthread_mutex_init(&processMutex, NULL);
@@ -26,14 +27,18 @@ class ProcessInterface{
 			comPoint = NULL;
 		};
 
-
+		/** Base-Desctructor.*/
 		virtual ~ProcessInterface()
 		{
 			pthread_mutex_destroy(&processMutex);
 			pthread_mutex_destroy(&busyMutex);
 		};
 
-
+		/*
+		 * Wrapper function for process(). It just calls process(),
+		 * catches unexpected exception and rethrows to the overlying ComPoint.
+		 * \note Mutex processMutex is used to protect the call of process().
+		 */
 		OutgoingMsg* processMsg(IncomingMsg* input)
 		{
 			OutgoingMsg* output = NULL;
@@ -59,7 +64,10 @@ class ProcessInterface{
 			return output;
 		}
 
-
+		/*
+		 * \return True if there is currently a message processing, else false.
+		 * \note Uses mutex busyMutex.
+		 */
 		bool isBusy()
 		{
 			bool result = false;
@@ -72,8 +80,15 @@ class ProcessInterface{
 
 	protected:
 
+		/** Corresponding comPoint. Will only be used if there is need
+		 * for subRequests during the process of a message.*/
 		ComPoint* comPoint;
 
+		/**
+		 * Sets the busy flag of this Interface to busy or not busy.
+		 * \param True for busy, false for not busy.
+		 * \note Uses mutex busyMutex.
+		 */
 		void setBusy(bool value)
 		{
 			pthread_mutex_lock(&busyMutex);
@@ -83,11 +98,20 @@ class ProcessInterface{
 
 
 	private:
+		/*! Mutex fo protecting the call of process().*/
 		pthread_mutex_t processMutex;
+		/*! Mutex to protect setting/getting busy flag status.*/
 		pthread_mutex_t busyMutex;
+		/*! Shows if a message is currently processing or not.*/
 		bool busy;
 
-
+		/*!
+		 * Definition of process(), which has to process an instance of IncommingMsg and can
+		 * return an instance of OutgoingMsg.
+		 * \param input The message which should be processed.
+		 * \return NULL if the input triggers asynchrounous features, else there will be a instance of OutgoingMsg with a response as return.
+		 * \note Classes which inherits from processInterface have to implement this function.
+		 */
 		virtual OutgoingMsg* process(IncomingMsg* input) = 0;
 
 };
--- include/ProcessInterfaceB.hpp
+++ include/ProcessInterfaceB.hpp
@@ -1,13 +1,5 @@
-/*
- * ProcessInterfaceB.hpp
- *
- *  Created on: 08.06.2015
- *      Author: dave2
- */
-
 #ifndef INCLUDE_ProcessInterfaceB_HPP_
 #define INCLUDE_ProcessInterfaceB_HPP_
-
 
 
 #include <pthread.h>
@@ -16,16 +8,30 @@
 #include "ProcessInterface.hpp"
 
 
+/**
+ * \interface ProcessInterfaceB
+ * Adds the definition of the pure virtual function isSubResponse and the function
+ * setComPoint(ComPoint*) to the baseclass ProcessInterface. These are necessary features
+ * if we want to send subRequests (over ComPoint) and receive subResponses (ComPointB using
+ * isSubResponse method within listener).
+ */
 class ProcessInterfaceB : public ProcessInterface{
 
 
 	public:
+		/** Base-Constructor.*/
 		ProcessInterfaceB(){};
 
+		/** Base-Desctructor.*/
 		virtual ~ProcessInterfaceB(){};
 
+		/** Setting the comPoint, so we can use it's transmit methods to send subRequests.*/
 		void setComPoint(ComPoint* comPoint){this->comPoint = comPoint;}
 
+		/** The implementation of this should analyse a incoming message if it is a response message
+		 * and if the json rpc id is the corresponding one of the send subRequest.
+		 * \note Has to be implemented by the class which inherits from ProcessInterfaceB.
+		 */
 		virtual bool isSubResponse(RPCMsg* rpcMsg) = 0;
 
 };
