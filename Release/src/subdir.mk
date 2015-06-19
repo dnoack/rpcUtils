19d8e3617c4df45aa8aad3cb710c57b2dd7766c6
dave2 <dave2@Magic2>
2015-06-19 10:33:58 +0200

    makefile variables...

--- Release/src/subdir.mk
+++ Release/src/subdir.mk
@@ -35,7 +35,7 @@ CPP_DEPS += \
 src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -I"/home/dave2/git/rpcUtils/include" -I/home/dave2/git/rapidjson/include/rapidjson -O3 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -I"/home/dave2/git/rpcUtils/include" -I/home/dave2/git/rapidjson/include/rapidjson -O3 -Wall -c -fmessage-length=0 ${CXXFLAGS} -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
